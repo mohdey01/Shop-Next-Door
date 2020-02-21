95aaa562b6aadcba6bc7a5d6edfe36bcf5b98fdb
Mohini Pandey <49555428+mohdey01@users.noreply.github.com>
2020-02-22 01:13:35 +0530

    added new migration file2

--- core/migrations/0012_auto_20190507_1228.py
+++ core/migrations/0012_auto_20190507_1228.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='billingaddress',
             name='country',
-            field=django_countries.fields.CountryField(default='China', max_length=2),
+            field=django_countries.fields.CountryField(default='China'),
             preserve_default=False,
         ),
     ]
