a7b716505f2647d4c9893e0ff1ca53ecd7b168e7
Mohini Pandey <49555428+mohdey01@users.noreply.github.com>
2020-02-22 21:21:53 +0530

    debug change2

--- ecommerce/settings/development.py
+++ ecommerce/settings/development.py
@@ -46,6 +46,39 @@ DATABASES = {
 
 STRIPE_PUBLIC_KEY = 'pk_test_YBfmkafeamYk3bJ2mEvWUx1I009H5ehRXO'
 STRIPE_SECRET_KEY = 'sk_test_JNK5OusND4My92qzHeqlY0Z100tXWMbcWs'
-
+#Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+DEBUG_PROPAGATE_EXCEPTIONS = True
 # Activate Django-Heroku.
 django_heroku.settings(locals())
