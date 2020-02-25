2374aeca048eed3378b0d7673a57c4b190554e7f
Mohini Pandey <49555428+mohdey01@users.noreply.github.com>
2020-02-25 21:04:29 +0530

    Commit on 25/2

--- ecommerce/settings/development.py
+++ ecommerce/settings/development.py
@@ -1,9 +1,41 @@
 from .base import *
 import django_heroku
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
+INSTALLED_APPS += [
+    'debug_toolbar'
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+# DEBUG TOOLBAR SETTINGS
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+}
 
 DATABASES = {
     'default': {
@@ -15,39 +47,5 @@ DATABASES = {
 STRIPE_PUBLIC_KEY = 'pk_test_YBfmkafeamYk3bJ2mEvWUx1I009H5ehRXO'
 STRIPE_SECRET_KEY = 'sk_test_JNK5OusND4My92qzHeqlY0Z100tXWMbcWs'
 
-#Logging
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
-                       'pathname=%(pathname)s lineno=%(lineno)s ' +
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'testlogger': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        }
-    }
-}
-DEBUG_PROPAGATE_EXCEPTIONS = True
 # Activate Django-Heroku.
 django_heroku.settings(locals())
